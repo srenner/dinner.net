56b76b284c096b60d0087aa94b6528b5aa00cf73
Shawn Renner <shawn.renner@gmail.com>
2013-12-10 10:04:51 -0600

    Initial commit of project template

---
+++ dinner.net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dinner.net.Startup))]
+namespace dinner.net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

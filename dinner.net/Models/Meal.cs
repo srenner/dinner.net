6e142c632b0346b5b96a49c3424d871ef72a32e9
Shawn Renner <shawn.renner@gmail.com>
2013-12-18 16:33:00 -0600

    Begin Meal model

---
+++ dinner.net/Models/Meal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace dinner.net.Models
+{
+    public class Meal
+    {
+        public int ID { get; set; }
+        
+        [Required(ErrorMessage="Meal must have a name")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        
+        [Display(Name = "Extended Description")]
+        public string ExtendedDescription { get; set; }
+
+
+        [Display(Name = "Steps to make")]
+        public string StepsToMake { get; set; }
+
+        [Display(Name = "Rating 1-10")]
+        public int Rating { get; set; }
+        
+        public DateTime LastAte { get; set; }
+    }
+}
---
+++ dinner.net/Models/MealIngredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dinner.net.Models
+{
+    public class MealIngredient
+    {
+        public int ID { get; set; }
+
+        public Meal Meal { get; set; }
+        public Ingredient Ingredient { get; set; }
+        public int Quantity { get; set; }
+        public string QuantityDescription { get; set; }
+    }
+}

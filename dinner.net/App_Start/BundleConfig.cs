868df1a723c894fb7ab8d15ab9682c3fecdc5d50
Shawn Renner <shawn.renner@gmail.com>
2013-12-23 09:59:26 -0600

    Include jQuery UI

--- dinner.net/App_Start/BundleConfig.cs
+++ dinner.net/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace dinner.net
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").IncludeDirectory(
+                        "~/Scripts/jquery-ui-1.10.3.custom/js", "*.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 

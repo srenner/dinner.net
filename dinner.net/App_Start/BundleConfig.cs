fa0559fd476001ba9f64c404bb7be59df6dfb923
Shawn Renner <shawn.renner@gmail.com>
2013-12-23 10:21:15 -0600

    js/css reorg and bundling

--- dinner.net/App_Start/BundleConfig.cs
+++ dinner.net/App_Start/BundleConfig.cs
@@ -11,8 +11,11 @@ namespace dinner.net
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").IncludeDirectory(
-                        "~/Scripts/jquery-ui-1.10.3.custom/js", "*.js"));
+            bundles.Add(new StyleBundle("~/Content/pepper-grinder").Include(
+                        "~/Content/pepper-grinder/jquery-ui-1.10.3.custom.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui-1.10.3.custom.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
